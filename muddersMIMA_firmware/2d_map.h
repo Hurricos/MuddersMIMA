e24799747ddad1847d1a2b7aed1121d81fa7321d
Martin Kennedy <hurricos@gmail.com>
2024-01-25 17:51:59 -0500

    debug: throttle ima power mapping: Add debug lines

--- muddersMIMA_firmware/2d_map.h
+++ muddersMIMA_firmware/2d_map.h
@@ -4,4 +4,5 @@
 	#define twod_map_h
 
 	uint8_t evaluate_2d_map(uint8_t* map, uint8_t* dim1_map, uint8_t dim1_size, uint8_t dim1, uint8_t* dim2_map, uint8_t dim2_size, uint8_t dim2);
+    uint8_t two_d_map_getLastUsedMapIndex();
 #endif
